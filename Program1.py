b36a1004fe2cc76c3fb46c3c7036e990446460ab
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-20 23:05:43 +0800

    PROGRAM FLOW: My first or initial plan for the program

---
+++ Program1.py
@@ -0,0 +1,42 @@
+# Number Sorter
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement.
+
+# ************Program Flow*************
+
+# my first code plan for ordering the list: reference from the internet
+# my_list = sorted(my_list, reverse=False) 
+# print(my_list)
+
+# ************Steps***************
+
+# 1. Ask for the first number.
+def first_input():
+    _first = int(input("\nPlease enter your first number: "))
+    return _first
+# Calling the function.
+first = first_input()
+
+# 2. Ask for the second number.
+def second_input(): 
+    _second = int(input("Please enter your second number: "))
+    return _second
+# Calling the function.
+second = second_input()
+
+# 3. Ask for the third number.
+def third_input():
+    _third = int(input("Please enter your third number: "))
+    return _third
+# Calling the function.
+third = third_input()
+
+# 4. Ask for the forth number.
+def forth_input():
+    _forth = int(input("Please enter your forth number:"))
+    return _forth
+# Calling the function.
+forth = forth_input()
+
+# ***********if-else Statements Section***********
+
