3011a9ef109f8a1243080ed5a2284e9fe50de6ba
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-21 17:53:04 +0800

    PROGRAM MODIFIED: Finished the multiple selectionsfor each condition but came up with an error

--- Program1.py
+++ Program1.py
@@ -136,7 +136,123 @@ def input_vl(first_num, second_num, third_num, forth_num):
                                                 # Time functionality for program beautification attempt.
                                                 time.sleep(3)
                                                 # Print the if result.
-       
+                                                print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+        else:
+            if third_num >= first_num and third_num >= second_num and third_num >=  forth_num:
+                if first_num >= second_num and first_num >= forth_num:
+                    if second_num >= forth_num: 
+                        # numerical equivalent of the order: [3,1,2,4]
+                        sorted_num = (third_num, first_num, second_num, forth_num)
+                        print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                        # Time functionality for program beautification attempt.
+                        time.sleep(3)
+                        # Print the if result.
+                        print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                    else: 
+                        if forth_num >= second_num:
+                            # numerical equivalent of the order: [3,1,4,2]
+                            sorted_num = (third_num, first_num, second_num, forth_num)
+                            print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                            # Time functionality for program beautification attempt.
+                            time.sleep(3)
+                            # Print the if result.
+                            print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                        else: 
+                            if second_num >= first_num and second_num >= forth_num:
+                                if first_num >= forth_num:
+                                    # numerical equivalent of the order: [3,2,1,4]
+                                    sorted_num = (third_num, second_num, first_num, forth_num)
+                                    print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                                    # Time functionality for program beautification attempt.
+                                    time.sleep(3)
+                                    # Print the if result.
+                                    print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                                else: 
+                                    if forth_num >= first_num:
+                                        # numerical equivalent of the order: [3,2,4,1]
+                                        sorted_num = (third_num, second_num, forth_num, first_num)
+                                        print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                                        # Time functionality for program beautification attempt.
+                                        time.sleep(3)
+                                        # Print the if result.
+                                        print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                                    else:
+                                        if forth_num >= first_num and forth_num >= second_num:
+                                            if first_num >= second_num:
+                                                # numerical equivalent of the order: [3,4,1,2]
+                                                sorted_num = (third_num, forth_num, second_num, first_num)
+                                                print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                                                # Time functionality for program beautification attempt.
+                                                time.sleep(3)
+                                                # Print the if result.
+                                                print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                                            else:
+                                                if second_num >= first_num:
+                                                    # numerical equivalent of the order: [3,4,2,1]
+                                                    sorted_num = (third_num, forth_num, second_num, first_num)
+                                                    print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                                                    # Time functionality for program beautification attempt.
+                                                    time.sleep(3)
+                                                    # Print the if result.
+                                                    print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+            else:
+                if forth_num >= first_num and forth_num >= second_num and forth_num >= third_num:
+                    if first_num >= second_num and first_num >= third_num:
+                        if second_num >= third_num: 
+                            # numerical equivalent of the order: [4,1,2,3]
+                            sorted_num = (forth_num, first_num, second_num, third_num)
+                            print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                            # Time functionality for program beautification attempt.
+                            time.sleep(3)
+                            # Print the if result.
+                            print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                        else: 
+                            if third_num >= second_num:
+                                # numerical equivalent of the order: [4,1,3,2]
+                                sorted_num = (forth_num, first_num, third_num, second_num)
+                                print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                                # Time functionality for program beautification attempt.
+                                time.sleep(3)
+                                # Print the if result.
+                                print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                            else: 
+                                if second_num >= first_num and second_num >= third_num:
+                                    if first_num >= third_num:
+                                        # numerical equivalent of the order: [4,2,1,3]
+                                        sorted_num = (forth_num, second_num, first_num, third_num)
+                                        print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                                        # Time functionality for program beautification attempt.
+                                        time.sleep(3)
+                                        # Print the if result.
+                                        print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                                    else: 
+                                        if third_num >= first_num:
+                                            # numerical equivalent of the order: [4,2,3,1]
+                                            sorted_num = (forth_num, second_num, third_num, first_num)
+                                            print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                                            # Time functionality for program beautification attempt.
+                                            time.sleep(3)
+                                            # Print the if result.
+                                            print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                                        else:
+                                            if third_num >= first_num and third_num >= second_num:
+                                                if first_num >= second_num:
+                                                    # numerical equivalent of the order: [4,3,1,2]
+                                                    sorted_num = (forth_num, third_num, first_num, second_num)
+                                                    print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                                                    # Time functionality for program beautification attempt.
+                                                    time.sleep(3)
+                                                    # Print the if result.
+                                                    print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                                                else:
+                                                    if second_num >= first_num:
+                                                        # numerical equivalent of the order: [4,3,2,1]
+                                                        sorted_num = (forth_num, third_num, second_num, first_num)
+                                                        print("\nLet's arrange those numbers in ascending order!        \n[PROCESSING...]")
+                                                        # Time functionality for program beautification attempt.
+                                                        time.sleep(3)
+                                                        # Print the if result.
+                                                        print(f"\nYey! The system arranged your numbers from highest to lowest: {sorted_num}")
 
 try:
     # Ask for the first number.
