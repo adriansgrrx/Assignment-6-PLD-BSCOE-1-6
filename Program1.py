4c6903f28c6b0efe29fcdf4fa02b2e8826d4a59f
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-21 12:21:26 +0800

    PROGRAM ENHANCED: Import time and value error addition, if-elsestatements and additional comments.

--- Program1.py
+++ Program1.py
@@ -3,40 +3,81 @@
 # Print the 4 numbers from highest to lowest using only if-else statement.
 
 # ************Program Flow*************
+"""""
+Upon analyzing, the whole program possesses the probability of four(4) input random numbers to be sorted
+from highest to lowest. There will be six(6) different set of orders to be process using relational operators
+using if-else statements.
+"""""
 
-# my first code plan for ordering the list: reference from the internet
-# my_list = sorted(my_list, reverse=False) 
-# print(my_list)
+import time
 
-# ************Steps***************
+# Greetings
+name = input("\nHey there!\nPlease state your name first: ")
+print(f"\nGreat! Let's proceeed now {name.title()}!")
+time.sleep(1)
+print("\nLoading...")
+time.sleep(3)
 
-# 1. Ask for the first number.
-def first_input():
-    _first = int(input("\nPlease enter your first number: "))
-    return _first
-# Calling the function.
-first = first_input()
 
-# 2. Ask for the second number.
-def second_input(): 
-    _second = int(input("Please enter your second number: "))
-    return _second
-# Calling the function.
-second = second_input()
+# ******def functionality including if - else statements section*****
+def input_vl(first_num, second_num, third_num, forth_num):
+    if first_num >= second_num and first_num >= third_num and first_num >= forth_num:
+        if second_num >= third_num and second_num >= forth_num:
+            if third_num >= forth_num:
+                # numerical equivalent of the order: [1,2,3,4]
+                sorted_num = [first_num, second_num, third_num, forth_num]
+                print("\nLet's arrange those numbers in ascending order! \n[PROCESSING...]")
+                # Time functionality for program beautification attempt.
+                time.sleep(3)
+                # Print the if result.
+                print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")        
+            else: 
+                if forth_num >= third_num:
+                    # numerical equivalent of the order: [1,2,4,3]
+                    sorted_num = (first_num, second_num, forth_num, third_num)
+                    print("\nLet's arrange those numbers in ascending order! \n[PROCESSING...])")
+                    # Time functionality for program beautification attempt.
+                    time.sleep(3)
+                    # Print the if result.
+                    print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                else:
+                    if third_num >= second_num and third_num >= forth_num:
+                        if second_num >= forth_num:
+                            # numerical equivalent of the order: [1,3,2,4]
+                            sorted_num = (first_num, third_num, second_num, forth_num)
+                            print("\nLet's arrange those numbers in ascending order! \n[PROCESSING...]")
+                            # Time functionality for program beautification attempt.
+                            time.sleep(3)
+                            # Print the if result.
+                            print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+                        else:
+                            if forth_num >= second_num:
+                            # numerical equivalent of the order: [1,3,4,2]
+                                sorted_num = (first_num, third_num, forth_num, second_num)
+                                print("\nLet's arrange those numbers in ascending order! \n[PROCESSING...]")
+                                # Time functionality for program beautification attempt.
+                                time.sleep(3)
+                                # Print the if result.
+                                print(f"\nYey! The system arranged your numbers from highest to lowest: [{sorted_num}]")
+try:
+    # 1. Ask for the first number.
+    first_num = int(input("\nPlease enter your first number: "))
 
-# 3. Ask for the third number.
-def third_input():
-    _third = int(input("Please enter your third number: "))
-    return _third
-# Calling the function.
-third = third_input()
+    # 2. Ask for the second number.
+    second_num = int(input("Please enter your second number: "))
 
-# 4. Ask for the forth number.
-def forth_input():
-    _forth = int(input("Please enter your forth number:"))
-    return _forth
-# Calling the function.
-forth = forth_input()
+    # 3. Ask for the third number.
+    third_num = int(input("Please enter your third number: "))
+
+    # 4. Ask for the forth number.
+    forth_num = int(input("Please enter your forth number: "))
+
+    input_vl(first_num, second_num, third_num, forth_num) 
+
+# adding except value to show invalid input
+except ValueError:
+    print("\n[ERROR] Invalid input! The system only accepts WHOLE NUMBERS.")
+    
 
 # ***********if-else Statements Section***********
-
+# variables = first_num, second_num, third_num, forth_num
