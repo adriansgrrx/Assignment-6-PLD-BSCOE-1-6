6f903992a0702c21b0b823738cd50447844c91c5
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-23 23:10:37 +0800

    PROGRAM UNCOMPLETE: Struggling on looping. My program can generate random numbers and get their sum but cant loop itself upto 10 times which isone of the task reqiurements.

--- Program2.py
+++ Program2.py
@@ -5,7 +5,8 @@
 # Display the result summary of the 10 operations (ex 9/10)
 
 """
-Random and Time modules were used in this program.
+On this program I will now use elif statement instead of if-else to reduce the program lines. 
+Random and Time modules were also used in this program. 
 """
 from random import randrange 
 import time
@@ -14,6 +15,10 @@ import time
 itemNo = 1
 # Max given items which is 10.
 itemMax = 10
+# Essential (global) scoring variables 
+rAnswers = 0    # r = (Right)Answers
+xAnswers = 0    # x = (Wrong)Answers
+
 
 # Two variables that generates two rando numbers to be add
 random_num1 = randrange(0,99)
@@ -24,11 +29,15 @@ sum = random_num1 + random_num2
 print(f"\n[Item no.{itemNo} out of {itemMax} questions]\n>>> {random_num1} + {random_num2} is equal to what number?\n")
 answer_input = input("<Enter your answer here>\nMy answer is: ")
 
-
 if int(answer_input) == sum:
     print("\nCorrect!")
+    rAnswers +=1
+    itemNo += 1
+
         
 elif int(answer_input) != sum: 
-        print("\nIncorrect!")
-        print(f"The correct answer is {sum}.")
+    print("\nIncorrect!")
+    print(f"The correct answer is {sum}.")
+    xAnswers +=1
+    itemNo += 1
         
