0dcc5ffc1a64113be6067d61f584a10ef4245a58
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-23 23:34:13 +0800

    PROGRAM 2 FIXED: After a while of research, I figure out the loop functionality to use. I also add time module and add some comments for explanation etc.

--- Program2.py
+++ Program2.py
@@ -19,25 +19,33 @@ itemMax = 10
 rAnswers = 0    # r = (Right)Answers
 xAnswers = 0    # x = (Wrong)Answers
 
+# Adding 'while' function for the loop of the if-elif statements.
+while itemNo <= itemMax:
+    # Two variables that generates two rando numbers to be add
+    random_num1 = randrange(0,99)
+    random_num2 = randrange(0,99)
 
-# Two variables that generates two rando numbers to be add
-random_num1 = randrange(0,99)
-random_num2 = randrange(0,99)
-
-sum = random_num1 + random_num2
-
-print(f"\n[Item no.{itemNo} out of {itemMax} questions]\n>>> {random_num1} + {random_num2} is equal to what number?\n")
-answer_input = input("<Enter your answer here>\nMy answer is: ")
-
-if int(answer_input) == sum:
-    print("\nCorrect!")
-    rAnswers +=1
-    itemNo += 1
-
-        
-elif int(answer_input) != sum: 
-    print("\nIncorrect!")
-    print(f"The correct answer is {sum}.")
-    xAnswers +=1
-    itemNo += 1
-        
+    sum = random_num1 + random_num2 # Add the two random numbers and store on a variable
+    # print the questioning statement that will facilitate the entire Addition quiz.
+    print(f"\n[Item no.{itemNo} out of {itemMax} questions]\n>>> {random_num1} + {random_num2} is equal to what number?\n")
+    answer_input = input("<Enter your answer here>\nMy answer is: ")    # user input stored in a variable
+    """
+    The 'time.sleep()' functionality serves as the transition per questions.
+    """
+    if answer_input.isdigit() == True: # Boolean expression to facilitate the if-elif statements and for Error Validation. 
+        if int(answer_input) == sum:
+            print("\nCorrect!")
+            rAnswers +=1 
+            itemNo += 1
+            time.sleep(1) 
+            print("\n[NEXT QUESTION AHEAD]")
+            time.sleep(2)
+        elif int(answer_input) != sum: 
+                print("\nIncorrect!")
+                print(f"The correct answer is {sum}.")
+                xAnswers +=1
+                itemNo += 1
+                time.sleep(1)
+                print("\n[NEXT QUESTION AHEAD]")
+                time.sleep(2)
+                
